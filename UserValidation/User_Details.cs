7c61ed95ad7ec498797f4f127b3492f3784bfffa
StudyAnkit <ankitghosh2018@gmail.com>
2022-04-01 13:49:26 +0530

    [Ankit]-User validation with sql server

---
+++ UserValidation/User_Details.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserValidation
+{
+    internal class User_Details
+    {
+
+        public string Username
+        {
+            get { return Username; }
+            set
+            {
+                string pattern = "^[A-Z]{1}[a-z]{2,} ^[A-Z]{1}[a-z]{2,}$";
+                if (Regex.IsMatch(value, pattern))
+                {
+                    Email = value;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid username");
+                }
+            }
+        }
+        public string Password
+        {
+            get { return Password; }
+            set
+            {
+                string pattern = "^(?=.*?[0-9])(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[@#$&*.!?]).{8,}";
+                if (Regex.IsMatch(value, pattern))
+                {
+                    Email = value;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid pattern");
+                }
+            }
+        }
+        public string Name { get; set; }
+        public string Email
+        {
+            get { return Email; }
+            set
+            {
+                string pattern = " ^[0-9a-zA-Z]+[.+-_]{0,1}[0-9a-zA-Z]+([@]cstech.in)$";
+                if (Regex.IsMatch(value, pattern))
+                {
+                    Email = value;
+                }
+                else
+                {
+                    Console.WriteLine("didn't match" );
+                }
+            }
+        }
+            
+        public long PhoneNumber { get; set; }
+        public string Designation { get; set; }
+        public string Gender { get; set; }
+        public string Course { get; set; }
+        
+    }
+}
---
+++ UserValidation/ValidationCheck.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace UserValidation
+{
+    internal class ValidationCheck
+    {
+        public static string connectionString = @"Data Source=(localdb)\ProjectModels;Initial Catalog=userlogin;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        SqlConnection connection = new SqlConnection(connectionString);
+        public void userEntryCheck()
+        {
+            connection.Open();
+            using (this.connection)
+            {
+                User_Details displayModel = new User_Details();
+                SqlCommand command = new SqlCommand("SpUserDetails", connection);
+
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@Name", displayModel.Name);
+                command.Parameters.AddWithValue("@Email", displayModel.Email);
+                command.Parameters.AddWithValue("@PhoneNumber", displayModel.PhoneNumber);
+                command.Parameters.AddWithValue("@Designation", displayModel.Designation);
+                command.Parameters.AddWithValue("@Course", displayModel.Course);
+                command.Parameters.AddWithValue("@Gender", displayModel.Course);
+                command.Parameters.AddWithValue("@Username", displayModel.Course);
+                command.Parameters.AddWithValue("@Password", displayModel.Course);
+
+                string query = $"Select * from employee_payroll where email is {displayModel.Email};";
+                SqlCommand validation_command = new SqlCommand(query, connection);
+
+                connection.Open();
+                SqlDataReader dr = command.ExecuteReader();
+                SqlDataReader dr_check = validation_command.ExecuteReader();
+                if (dr_check.HasRows)
+                {
+                    Console.WriteLine("Validation failed");
+                }
+                else
+                {
+
+                    if (dr.HasRows)
+                    {
+                        while (dr.Read())
+                        {
+                            displayModel.Name = dr["Name"].ToString();
+
+                            displayModel.Email = dr["Email"].ToString();
+
+                            displayModel.PhoneNumber = Convert.ToInt32(dr["PhoneNumber"]);
+
+                            displayModel.Designation = dr["Designation"].ToString();
+
+                            displayModel.Course = dr["Course"].ToString();
+
+                            displayModel.Gender = dr["Gender"].ToString();
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No data found.");
+                    }
+                }
+
+                string query_LoginCheck = $"Select * from employee_payroll where Username is {displayModel.Email};";
+                connection.Close();
+            }
+        }
+    }
+}
+
